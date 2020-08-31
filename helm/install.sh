019b3b4add6f75fe907a20b8e3f67e06503c298e
samostrovskyi <senegalelastico@gmail.com>
2020-08-31 19:21:44 +0300

    monitoring and logging charts

--- helm/install.sh
+++ helm/install.sh
@@ -6,3 +6,8 @@ for chart in {"arangodb","kafka","q-server","statsd","ton-node"}; do
   helm install -n $1 $chart ./$chart
   echo "=================================="
 done
+
+#helm repo add stable https://kubernetes-charts.storage.googleapis.com
+#helm install grafana -n sam-test stable/grafana
+
+#helm install -n sam-test graylog stable/graylog
