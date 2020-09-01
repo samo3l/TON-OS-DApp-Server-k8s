e34c6f2d309bdbf531286dfcfb46a8e28561b75a
samostrovskyi <senegalelastico@gmail.com>
2020-09-01 14:17:30 +0300

    configmap vars

--- helm/node_install.sh
+++ helm/node_install.sh
@@ -3,11 +3,11 @@
 kubectl create ns $1 || true
 
 for chart in {"arangodb","kafka","q-server","statsd","ton-node"}; do 
-  helm install -n $1 $chart ./$chart
+  helm install -n $1 $chart ./charts/$chart
   echo "=================================="
 done
 
 #helm repo add stable https://kubernetes-charts.storage.googleapis.com
-#helm install grafana -n sam-test stable/grafana
 
-#helm install -n sam-test graylog stable/graylog
+#helm install -n $1 grafana stable/grafana
+#helm install -n $1 graylog stable/graylog
--- helm/node_uninstall.sh
+++ helm/node_uninstall.sh
@@ -4,3 +4,6 @@ for chart in {"arangodb","kafka","q-server","statsd","ton-node"}; do
   helm uninstall -n $1 $chart
   echo "=================================="
 done
+
+helm uninstall -n $1 grafana
+helm uninstall -n $1 graylog
